97d4f99eccc5c512f09739d62f1997d2ea5a7104
Amanda on Mona <amanda@velociraptor.info>
2015-08-20 21:07:08 -0700

    Overkill on the PEP8, probably.

--- website/frontend/views.py
+++ website/frontend/views.py
@@ -13,25 +13,26 @@ import time
 from django.template import Context, RequestContext, loader
 from django.views.decorators.cache import cache_page
 
-OUT_FORMAT = '%B %d, %Y at %l:%M%P EDT'
+OUT_FORMAT = '%B %d, %Y at %l: %M%P EDT'
 
 SEARCH_ENGINES = """
-http://www.ask.com
-http://www.google
-https://www.google
+http: //www.ask.com
+http: //www.google
+https: //www.google
 search.yahoo.com
-http://www.bing.com
+http: //www.bing.com
 """.split()
+
 
 def came_from_search_engine(request):
     return any(x in request.META.get('HTTP_REFERER', '')
                for x in SEARCH_ENGINES)
 
 
-
 def Http400():
     t = loader.get_template('404.html')
     return HttpResponse(t.render(Context()), status=400)
+
 
 def get_first_update(source):
     if source is None:
@@ -43,6 +44,7 @@ def get_first_update(source):
     except IndexError:
         return datetime.datetime.now()
 
+
 def get_last_update(source):
     if source is None:
         source = ''
@@ -52,9 +54,10 @@ def get_last_update(source):
     except IndexError:
         return datetime.datetime.now()
 
+
 def get_articles(source=None, distance=0):
     articles = []
-    rx = re.compile(r'^https?://(?:[^/]*\.)%s/' % source if source else '')
+    rx = re.compile(r'^https?: //(?: [^/]*\.)%s/' % source if source else '')
 
     pagelength = datetime.timedelta(days=1)
     end_date = datetime.datetime.now() - distance * pagelength
@@ -81,9 +84,10 @@ def get_articles(source=None, distance=0):
                                               (start_date, end_date))
     article_dict = {}
     for v in all_versions:
-        a=models.Article(id=v.article_id,
-                         url=v.a_url, initial_date=v.a_initial_date,
-                         last_update=v.a_last_update, last_check=v.a_last_check)
+        a = models.Article(id=v.article_id, url=v.a_url,
+                           initial_date=v.a_initial_date,
+                           last_update=v.a_last_update,
+                           last_check=v.a_last_check)
         v.article = a
         article_dict.setdefault(v.article, []).append(v)
 
@@ -92,30 +96,32 @@ def get_articles(source=None, distance=0):
         if not rx.match(url):
             print 'REJECTING', url
             continue
-        if 'blogs.nytimes.com' in url: #XXX temporary
+        if 'blogs.nytimes.com' in url:  # XXX temporary
             continue
 
         if len(versions) < 2:
             continue
         rowinfo = get_rowinfo(article, versions)
         articles.append((article, versions[-1], rowinfo))
-    print 'Queries:', len(django.db.connection.queries), django.db.connection.queries
-    articles.sort(key = lambda x: x[-1][0][1].date, reverse=True)
+    print 'Queries: ', len(django.db.connection.queries), django.db.connection.queries
+    articles.sort(key=lambda x: x[-1][0][1].date, reverse=True)
     return articles
 
 
 SOURCES = '''nytimes.com cnn.com politico.com washingtonpost.com
-bbc.co.uk'''.split()
+bbc.co.uk sfpublicpress.org'''.split()
+
 
 def is_valid_domain(domain):
     """Cheap method to tell whether a domain is being tracked."""
     return any(domain.endswith(source) for source in SOURCES)
 
-@cache_page(60 * 30)  #30 minute cache
+
+@cache_page(60 * 30)  # 30 minute cache
 def browse(request, source=''):
     if source not in SOURCES + ['']:
         raise Http404
-    pagestr=request.REQUEST.get('page', '1')
+    pagestr = request.REQUEST.get('page', '1')
     try:
         page = int(pagestr)
     except ValueError:
@@ -123,22 +129,22 @@ def browse(request, source=''):
 
     first_update = get_first_update(source)
     num_pages = (datetime.datetime.now() - first_update).days + 1
-    page_list=range(1, 1+num_pages)
+    page_list = range(1, 1+num_pages)
 
     articles = get_articles(source=source, distance=page-1)
-    return render_to_response('browse.html', {
-            'source': source, 'articles': articles,
-            'page':page,
-            'page_list': page_list,
-            'first_update': first_update,
-            'sources': SOURCES
-            })
+    return render_to_response('browse.html', {'source': source,
+                                              'articles': articles,
+                                              'page': page,
+                                              'page_list': page_list,
+                                              'first_update': first_update,
+                                              'sources': SOURCES})
 
-@cache_page(60 * 30)  #30 minute cache
+
+@cache_page(60 * 30)  # 30 minute cache
 def feed(request, source=''):
     if source not in SOURCES + ['']:
         raise Http404
-    pagestr=request.REQUEST.get('page', '1')
+    pagestr = request.REQUEST.get('page', '1')
     try:
         page = int(pagestr)
     except ValueError:
@@ -147,19 +153,19 @@ def feed(request, source=''):
     first_update = get_first_update(source)
     last_update = get_last_update(source)
     num_pages = (datetime.datetime.now() - first_update).days + 1
-    page_list=range(1, 1+num_pages)
+    page_list = range(1, 1+num_pages)
 
     articles = get_articles(source=source, distance=page-1)
     return render_to_response('feed.xml', {
-            'source': source, 'articles': articles,
-            'page':page,
-            'request':request,
-            'page_list': page_list,
-            'last_update': last_update,
-            'sources': SOURCES
-            },
-            context_instance=RequestContext(request),
-            mimetype='application/atom+xml')
+        'source': source, 'articles': articles,
+        'page': page,
+        'request': request,
+        'page_list': page_list,
+        'last_update': last_update,
+        'sources': SOURCES},
+        context_instance=RequestContext(request),
+        mimetype='application/atom+xml')
+
 
 def old_diffview(request):
     """Support for legacy diff urls"""
@@ -214,12 +220,11 @@ def diffview(request, vid1, vid2, urlarg):
 
         indices = [i for i, x in versions.items() if x == v]
         if not indices:
-            #One of these versions doesn't exist / is boring
+            # One of these versions doesn't exist / is boring
             return Http400()
         index = indices[0]
         adjacent_versions.append([versions.get(index+offset)
                                   for offset in (-1, 1)])
-
 
     if any(x is None for x in texts):
         return Http400()
@@ -235,14 +240,15 @@ def diffview(request, vid1, vid2, urlarg):
             links.append('')
 
     return render_to_response('diffview.html', {
-            'title': title,
-            'date1':dates[0], 'date2':dates[1],
-            'text1':texts[0], 'text2':texts[1],
-            'prev':links[0], 'next':links[1],
-            'article_shorturl': article.filename(),
-            'article_url': article.url, 'v1': v1, 'v2': v2,
-            'display_search_banner': came_from_search_engine(request),
-            })
+        'title': title,
+        'date1': dates[0], 'date2': dates[1],
+        'text1': texts[0], 'text2': texts[1],
+        'prev': links[0], 'next': links[1],
+        'article_shorturl': article.filename(),
+        'article_url': article.url, 'v1': v1, 'v2': v2,
+        'display_search_banner': came_from_search_engine(request),
+        })
+
 
 def get_rowinfo(article, version_lst=None):
     if version_lst is None:
@@ -270,25 +276,25 @@ def prepend_http(url):
     url may look like
 
     www.nytimes.com
-    https:/www.nytimes.com    <- because double slashes get stripped
-    http://www.nytimes.com
+    https: /www.nytimes.com    <- because double slashes get stripped
+    http: //www.nytimes.com
     """
     components = url.split('/', 2)
     if len(components) <= 2 or '.' in components[0]:
-        components = ['http:', '']+components
+        components = ['http: ', '']+components
     elif components[1]:
-        components[1:1] = ['']
+        components[1: 1] = ['']
     return '/'.join(components)
 
 
 def article_history(request, urlarg=''):
-    url = request.REQUEST.get('url') # this is the deprecated interface.
+    url = request.REQUEST.get('url')  # this is the deprecated interface.
     if url is None:
         url = urlarg
     if len(url) == 0:
         return HttpResponseRedirect(reverse(front))
 
-    url = url.split('?')[0]  #For if user copy-pastes from news site
+    url = url.split('?')[0]  # For if user copy-pastes from news site
 
     url = prepend_http(url)
 
@@ -301,7 +307,6 @@ def article_history(request, urlarg=''):
     domain = url.split('/')[2]
     if not is_valid_domain(domain):
         return render_to_response('article_history_missing.html', {'url': url})
-
 
     try:
         article = Article.objects.get(url=url)
@@ -316,31 +321,34 @@ def article_history(request, urlarg=''):
         return HttpResponseRedirect(reverse(article_history, args=[article.filename()]))
 
     rowinfo = get_rowinfo(article)
-    return render_to_response('article_history.html', {'article':article,
-                                                       'versions':rowinfo,
-            'display_search_banner': came_from_search_engine(request),
+    return render_to_response('article_history.html', {'article': article,
+                                                       'versions': rowinfo,
+                                                       'display_search_banner': came_from_search_engine(request),
                                                        })
+
+
 def article_history_feed(request, url=''):
     url = prepend_http(url)
     article = get_object_or_404(Article, url=url)
     rowinfo = get_rowinfo(article)
     return render_to_response('article_history.xml',
-                              { 'article': article,
-                                'versions': rowinfo,
-                                'request': request,
-                                },
+                              {'article': article,
+                               'versions': rowinfo,
+                               'request': request, },
                               context_instance=RequestContext(request),
                               mimetype='application/atom+xml')
+
 
 def json_view(request, vid):
     version = get_object_or_404(Version, id=int(vid))
     data = dict(
         title=version.title,
-        byline = version.byline,
-        date = version.date.isoformat(),
-        text = version.text(),
+        byline=version.byline,
+        date=version.date.isoformat(),
+        text=version.text(),
         )
     return HttpResponse(json.dumps(data), mimetype="application/json")
+
 
 def upvote(request):
     article_url = request.REQUEST.get('article_url')
@@ -351,21 +359,26 @@ def upvote(request):
     models.Upvote(article_id=article_id, diff_v1=diff_v1, diff_v2=diff_v2, creation_time=datetime.datetime.now(), upvoter_ip=remote_ip).save()
     return render_to_response('upvote.html')
 
+
 def about(request):
     return render_to_response('about.html', {})
+
 
 def examples(request):
     return render_to_response('examples.html', {})
 
+
 def contact(request):
     return render_to_response('contact.html', {})
+
 
 def front(request):
     return render_to_response('front.html', {'sources': SOURCES})
 
+
 def subscribe(request):
     return render_to_response('subscribe.html', {})
 
+
 def press(request):
     return render_to_response('press.html', {})
-
