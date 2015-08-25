e2e9f2f2085e7bdb93b63d1021df8aaee5978b4a
Amanda on Mona <amanda@velociraptor.info>
2015-08-24 18:08:01 -0700

    PEP8

--- parsers/sfpp.py
+++ parsers/sfpp.py
@@ -2,21 +2,25 @@ from baseparser import BaseParser
 import bs4
 import re
 
+
 class SFPPParser(BaseParser):
+    '''
+    Look for links matching the "feeder_pat" regular expression
+    on any feeder_pages
+    '''
     domains = ['www.sfpublicpress.org']
 
-    feeder_pat   = '^http://sfpublicpress.org/news/searise/2015-07/' # Look for links matching this regular expression
-    feeder_pages = ['http://sfpublicpress.org/searise'] # on these pages
+    feeder_pat = '^http://sfpublicpress.org/news/searise/2015-07/'
+    feeder_pages = ['http://sfpublicpress.org/searise']
 
     def _parse(self, html):
-            soup = bs4.BeautifulSoup(html)
-            self.meta = soup.findAll('meta')
-            self.title = soup.find('h1', attrs={'class':'title'}).string
-            self.byline = soup.find(attrs={'class':'field-field-authors'}).text
-            self.byline = re.sub('[\s]+', ' ', self.byline).strip()
-            self.date = soup.find('div', attrs={'class':'date-created'}).string
-            self.date = re.sub('[\s]+', ' ', self.date).strip()
-            p_tags = soup.findAll('p')[2:]
-            real_p_tags = [p for p in p_tags if
-                         not p.findAll(attrs={'class':"twitter-follow-button"})]
-            self.body = '\n'+'\n\n'.join([p.getText() for p in real_p_tags])
+        soup = bs4.BeautifulSoup(html)
+        self.meta = soup.findAll('meta')
+        self.title = soup.find('h1', attrs={'class': 'title'}).string
+        self.byline = soup.find(attrs={'class': 'field-field-authors'}).text
+        self.byline = re.sub('[\s]+', ' ', self.byline).strip()
+        self.date = soup.find('div', attrs={'class': 'date-created'}).string
+        self.date = re.sub('[\s]+', ' ', self.date).strip()
+        p_tags = soup.findAll('p')[2:]
+        real_p_tags = [p for p in p_tags if not p.findAll(attrs={'class': "twitter-follow-button"})]
+        self.body = '\n'+'\n\n'.join([p.getText() for p in real_p_tags])
