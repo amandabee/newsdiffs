1c98d7cdad947f8fe5c345b99b4c872324964ca8
Amanda on Mona <amanda@velociraptor.info>
2015-08-24 18:08:45 -0700

    Adding an SFGate parser to the mix. just for kicks.

---
+++ parsers/sfgate.py
@@ -0,0 +1,27 @@
+from baseparser import BaseParser
+import bs4
+import re
+
+
+class SFGateParser(BaseParser):
+    '''
+    Look for links matching the "feeder_pat" regular expression
+    on any feeder_pages
+    '''
+    domains = ['www.sfgate.com']
+
+    feeder_pat = '^http://www.sfgate.com/bayarea'
+    feeder_pages = ['http://www.sfgate.com/bayarea/']
+
+    def _parse(self, html):
+        soup = bs4.BeautifulSoup(html)
+        self.meta = soup.findAll('meta')
+        self.title = soup.find('h1', attrs={'class': 'headline'}).string
+        self.byline = soup.find(attrs={'class': 'byline'}).text
+        self.byline = re.sub('[\s]+', ' ', self.byline).strip()
+        self.date = soup.find('h5', attrs={'class': 'timestamp'}).string
+        self.date = re.sub('[\s]+', ' ', self.date).strip()
+        p_tags = soup.findAll('p')[2:]
+        real_p_tags = [p for p in p_tags if
+                     not p.findAll(attrs={'class': "twitter-follow-button"})]
+        self.body = '\n'+'\n\n'.join([p.getText() for p in real_p_tags])
